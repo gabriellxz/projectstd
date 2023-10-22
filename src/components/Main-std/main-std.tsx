305eb5ae87df8b439322a259c929ae014aa6fe50
Gabriel <gabrielsilvaoliveira521@gmail.com>
2023-10-21 23:32:12 -0300

    update responsive

--- src/components/Main-std/main-std.tsx
+++ src/components/Main-std/main-std.tsx
@@ -5,24 +5,32 @@ import GROUP from '../../assets/Group-754-min.png'
 export default function MainStd() {
     return (
         <div className='container-main'>
-            <div className='container-img-group-std' id="sobre">
-                <img src={GROUP} alt="group" />
-            </div>
-            <div className='container-text-main-std'>
-                <div>
-                    <h3>
-                        Sobre <span>Nós</span>
-                        <div className='line-style'></div>
-                    </h3>
-                    <div className='container-p-std'>
-                        <p>
-                            Somos especialistas em soluções inteligentes, nossos estandes são ecologicamente corretos e adaptados às necessidades do meio ambiente.
-                        </p>
-                        <p>
-                            Desde 1998, criamos e executamos projetos e montagens de estandes com tecnologia, criatividade e profissionalismo, aliados à pontualidade, qualidade e segurança à serviço de nossos clientes.
-                        </p>
+            <div className='container-content-main'>
+                <div className='container-img-group-std' id="sobre">
+                    <img src={GROUP} alt="group" />
+                </div>
+                <div className='container-text-main-std'>
+                    <div>
+                        <h3>
+                            Sobre <span>Nós</span>
+                            <div className="container-line-style">
+                                <div className='line-style-two'></div>
+                                <div className='line-style-three'></div>
+                            </div>
+                        </h3>
+                        <div className='container-p-std'>
+                            <p>
+                                Somos especialistas em soluções inteligentes, nossos estandes são ecologicamente corretos e adaptados às necessidades do meio ambiente.
+                            </p>
+                            <p>
+                                Desde 1998, criamos e executamos projetos e montagens de estandes com tecnologia, criatividade e profissionalismo, aliados à pontualidade, qualidade e segurança à serviço de nossos clientes.
+                            </p>
+                        </div>
+                        <div className='container-button-main-std'>
+                            <div className='line-style'></div>
+                            <BtnPrimary textbtn='SAIBA MAIS' />
+                        </div>
                     </div>
-                    <BtnPrimary textbtn='SAIBA MAIS'/>
                 </div>
             </div>
         </div>
--- src/components/Section-contact/section-contact.tsx
+++ src/components/Section-contact/section-contact.tsx
@@ -6,32 +6,45 @@ export default function SectionContact() {
     return (
         <>
             <div>
-                <div className='container-title' id='contato'>
-                    <h2>
-                        Contato
-                        <div className='line-style'></div>
-                    </h2>
+                <div className='container-title-contact'>
+                    <div>
+                        <h2>Contato</h2>
+                        <div className="container-line-style">
+                            <div className="line-style-two"></div>
+                            <div className="line-style-three"></div>
+                        </div>
+                    </div>
                 </div>
-                <div className='container-form'>
-                    <form>
+                <div className='container-content-contact'>
+                    <div className="container-form">
+                        <form>
+                            <div className="container-input">
+                                <input type="text" placeholder='Nome' className='input'/>
+                            </div>
+                            <div className="container-input">
+                                <input type="text" placeholder='Telefone | Whatsapp' className='input'/>
+                            </div>
+                            <div className="container-input">
+                                <input type="email" placeholder='Email' className='input'/>
+                            </div>
+                            <div className="container-input">
+                                <input type="text" placeholder='Cidade | Estado' className='input'/>
+                            </div>
+                            <div className="container-input">
+                                <textarea name="" id="" cols={30} rows={10} placeholder='Mensagem'></textarea>
+                            </div>
+                            <div className="container-input">
+                                <button className="btn-contact">
+                                    enviar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="container-location">
                         <div>
-                            <input type="text" placeholder='Nome' />
+                            <iframe loading="lazy" src="https://maps.google.com/maps?q=Rua%20Ana%20L%C3%BAcia%20Dias%2C%20101%20Fortaleza&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near" title="Rua Ana Lúcia Dias, 101 Fortaleza" aria-label="Rua Ana Lúcia Dias, 101 Fortaleza"></iframe>
+                            <LocalizationStd />
                         </div>
-                        <div>
-                            <input type="text" placeholder='Telefone | Whatsapp' />
-                        </div>
-                        <div>
-                            <input type="email" placeholder='Email' />
-                        </div>
-                        <div>
-                            <input type="text" placeholder='Cidade | Estado' />
-                        </div>
-                        <textarea name="" id="" cols={30} rows={10} placeholder='Mensagem'></textarea>
-                        <input type="submit" value="Enviar" />
-                    </form>
-                    <div className='container-location'>
-                        <iframe loading="lazy" src="https://maps.google.com/maps?q=Rua%20Ana%20L%C3%BAcia%20Dias%2C%20101%20Fortaleza&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near" title="Rua Ana Lúcia Dias, 101 Fortaleza" aria-label="Rua Ana Lúcia Dias, 101 Fortaleza"></iframe>
-                        <LocalizationStd/>
                     </div>
                 </div>
             </div>
