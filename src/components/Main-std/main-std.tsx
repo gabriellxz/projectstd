864640159220591a7abb6717d992ef3bbbc4dce1
Gabriel <gabrielsilvaoliveira521@gmail.com>
2023-10-25 13:52:06 -0300

    modal

--- src/components/Main-std/main-std.tsx
+++ src/components/Main-std/main-std.tsx
@@ -1,10 +1,16 @@
 import './main.std.css'
 import BtnPrimary from '../Btn-primary/btn-primary'
 import GROUP from '../../assets/Group-754-min.png'
+import useModal from '../../hook/useModal'
+import ModalForm from '../Modal-form/modal-form'
 
 export default function MainStd() {
+
+    const { open, setOpen } = useModal()
+
     return (
         <div className='container-main'>
+            {open && <ModalForm modalFunction={() => setOpen(!open)}/>}
             <div className='container-content-main'>
                 <div className='container-img-group-std' id="sobre">
                     <img src={GROUP} alt="group" />
@@ -28,7 +34,7 @@ export default function MainStd() {
                         </div>
                         <div className='container-button-main-std'>
                             <div className='line-style'></div>
-                            <BtnPrimary textbtn='FALE CONOSCO'/>
+                            <BtnPrimary textbtn='FALE CONOSCO' modalFunction={() => setOpen(!open)}/>
                         </div>
                     </div>
                 </div>
